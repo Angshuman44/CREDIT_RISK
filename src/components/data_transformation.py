740ba2b4eedac0b2906af683d0a33fdb0e5fb092
Angshuman44 <mandalzzme@gmail.com>
2024-03-20 17:48:50 +0530

    Fourth commit has all data ingestion and all pipelines

---
+++ src/components/data_transformation.py
@@ -0,0 +1,119 @@
+import sys
+from dataclasses import dataclass
+from imblearn.over_sampling import SMOTE
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler,MaxAbsScaler
+from sklearn.preprocessing import LabelEncoder
+
+from src.utils import save_object
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path= os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config= DataTransformationConfig()
+
+    def get_data_transformer_object(self,train_df,target_column_name):
+        '''
+        this function is responsible for data transformation
+        '''
+        try:
+            numerical_columns= [col for col in train_df.columns if col != target_column_name and train_df[col].dtype != object]
+
+            categorical_columns = [col for col in train_df.columns if col != target_column_name and train_df[col].dtype == object]
+            print(numerical_columns)
+            print("")
+            print(categorical_columns)
+            num_pipeline=Pipeline(steps=[
+                ("imputer",SimpleImputer(strategy='median')),
+                ('scalar',MaxAbsScaler())
+
+            ])
+            cat_pipeline=Pipeline(steps=[
+            ("imputer",SimpleImputer(strategy="most_frequent")),
+            ("one_hot_encoder",OneHotEncoder()),
+            ("scaler",MaxAbsScaler())
+            ])
+
+            logging.info(f"Categorical Columns:{categorical_columns}")
+            logging.info(f"Numerical Columns:{numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ]
+
+            )
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+            
+    def initiate_data_transormation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Reading the train and test file")
+
+            target_column_name= 'label'
+            preprocessing_obj=self.get_data_transformer_object(train_df,target_column_name)
+
+            ## divide the train dataset to independent and dependent feature
+
+            input_features_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            ## divide the test dataset to independent and dependent feature
+
+            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+
+            logging.info("Applying Preprocessing on training and test dataframe")
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_features_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            # Apply SMOTE oversampling on the training data
+            smote = SMOTE()
+            input_feature_train_arr, target_feature_train_df = smote.fit_resample(input_feature_train_arr, target_feature_train_df)
+
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object")
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            return (
+
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+# if __name__=='__main__':
+#     obj=DataTransformation()
+#     obj.initiate_data_transormation('artifacts/train.csv','artifacts/test.csv')
